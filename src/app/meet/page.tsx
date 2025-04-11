7293777fcb0505e9f851f4033eb1a3e640d4537c
Anthony Cueva <acueva@pcienergysolutions.com>
2025-04-11 13:21:41 -0500

    refactor: update metadata formatting and add metadataBase to MeetPage

--- src/app/meet/page.tsx
+++ src/app/meet/page.tsx
@@ -1,23 +1,24 @@
-import { Metadata } from 'next'
-import { redirect } from 'next/navigation'
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: 'Schedule a Meeting with Anthony',
-  description: 'Book a time to meet with Anthony',
+  title: "Schedule a Meeting with Anthony",
+  description: "Book a time to meet with Anthony",
   openGraph: {
-    title: 'Schedule a Meeting with Anthony',
-    description: 'Book a time to meet with Anthony',
+    title: "Schedule a Meeting with Anthony",
+    description: "Book a time to meet with Anthony",
     images: [
       {
-        url: '/meet.png',
+        url: "/meet.png",
         width: 1200,
         height: 630,
-        alt: 'Schedule a Meeting',
+        alt: "Schedule a Meeting",
       },
     ],
   },
-}
+  metadataBase: new URL("https://cueva.io"),
+};
 
 export default function MeetPage() {
-  redirect('https://calendar.notion.so/meet/anthony-9n45j1rcc/4i9ij3lme')
-} 
+  redirect("https://calendar.notion.so/meet/anthony-9n45j1rcc/4i9ij3lme");
+}
