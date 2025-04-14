bf603a39d61b0313f968071c1dba78c0809d402e
Anthony Cueva <acueva@pcienergysolutions.com>
2025-04-13 20:38:04 -0500

    feat: add /movies endpoint with redirect and metadata for improved user experience and SEO

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,13 +8,21 @@ export function middleware(request: NextRequest) {
     request.nextUrl.pathname === "/meet" ||
     request.nextUrl.pathname === "/cal" ||
     request.nextUrl.pathname === "/calendar" ||
-    request.nextUrl.pathname === "/call"
+    request.nextUrl.pathname === "/call" ||
+    request.nextUrl.pathname === "/movies"
   ) {
     // Check if it's a bot
     if (detectBot(request)) {
       // For bots, let them access the page
       return NextResponse.next();
     } else {
+      if (request.nextUrl.pathname === "/movies") {
+        // For users, redirect to the Google Docs
+        return NextResponse.redirect(
+          "https://docs.google.com/document/d/1pBCgOaqG1s9KEYl0vrcv42M8FrQbOrtv5r4onXRbOEI/edit?usp=sharing"
+        );
+      }
+
       // For users, redirect to the Notion calendar
       return NextResponse.redirect(
         "https://calendar.notion.so/meet/anthony-9n45j1rcc/4i9ij3lme"
@@ -26,5 +34,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/meet", "/cal", "/calendar", "/call"],
+  matcher: ["/meet", "/cal", "/calendar", "/call", "/movies"],
 };
